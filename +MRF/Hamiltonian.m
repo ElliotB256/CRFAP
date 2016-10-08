cca1109d400513f64b94291b0de1ea409f8fce5c
unknown <bentine@ALWSDAQ71.physics.ox.ac.uk>
2016-10-08 10:41:24 +0100

    Added F=2 support to MRF Hamiltonian

--- +MRF/Hamiltonian.m
+++ +MRF/Hamiltonian.m
@@ -1,11 +1,20 @@
-function [ H ] = Hamiltonian( B0, RFs, Rabi, varargin )
+function [ H ] = Hamiltonian( Zs, RFs, Rabi, varargin )
 %HAMILTONIAN Calculates the Hamiltonian for a MRF system described by the
 %specified parameters.
-% Syntax: Hamiltonian( B0, RFs, Rabi )
-%  Bs: energy splitting of the undressed Zeeman states in MHz.
+% Syntax: Hamiltonian( Zs, RFs, Rabi )
+%  Zs: energy splitting of the undressed Zeeman states in MHz.
 %  RFs: vector of dressing RFs (MHz)
 %  Rabi: vector of dressing RF Rabi frequencies (MHz)
 %  Optional parameters: the values of alpha, Beta, zeta / Rabi
+
+p = inputParser;
+addRequired(p,'Zs',@isnumeric);
+addRequired(p,'RFs',@isnumeric);
+addRequired(p,'Rabi',@isnumeric);
+addParameter(p,'F', 1, @(x) any(ismember(x,[1 2])));
+
+parse(p,Zs, RFs, Rabi, varargin{:});
+
 
 % c = Rabi*2;
 % H = @(t) [ B0 , sum(c .* cos(RFs .* t), 1) / 2.^0.5, 0;
@@ -16,11 +25,30 @@ function [ H ] = Hamiltonian( B0, RFs, Rabi, varargin )
 e = exp(1);
 c = Rabi;
 
-H = @(t) [ ...
-    B0, sum( c .* e .^ (-RFs .* 1i .* t), 1) / 2.^0.5, 0;
-    sum( c .* e .^ (RFs .* 1i .* t), 1) / 2.^0.5, 0, sum( c .* e .^ (-RFs .* 1i .* t), 1) / 2.^0.5;
-    0, sum( c .* e .^ (RFs .* 1i .* t), 1) / 2.^0.5, -B0;
-    ];
-    
+switch F
+    case 1
+        
+        H = @(t) [ ...
+            Zs, sum( c .* e .^ (-RFs .* 1i .* t), 1) / 2.^0.5, 0;
+            sum( c .* e .^ (RFs .* 1i .* t), 1) / 2.^0.5, 0, sum( c .* e .^ (-RFs .* 1i .* t), 1) / 2.^0.5;
+            0, sum( c .* e .^ (RFs .* 1i .* t), 1) / 2.^0.5, -Zs;
+            ];
+        
+    case 2
+        
+        % s = sign of exponent
+        omega = @(t,s) sum( c .* e .^ (s*RFs .* 1i .* t), 1) / 2.^0.5;
+        H = @(t) [ ...
+                   2*Zs,    omega(t,-1),              0,              0,              0;
+            omega(t, 1),             Zs,    omega(t,-1),              0,              0;
+                      0,     omega(t,1),              0,    omega(t,-1),              0;
+                      0,              0,     omega(t,1),            -Zs,    omega(t,-1);
+                      0,              0,              0,     omega(t,1),          -2*Zs;
+                 ];
+        
+    otherwise
+        error('Unsupported value of F specified.');
+end
+
 end
 
