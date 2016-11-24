6f21fdee1e89755d48856280ad6a37b9c7d1e5c0
unknown <bentine@ALWSDAQ71.physics.ox.ac.uk>
2016-11-24 09:48:11 +0000

    Deprecated old SRF-only functions and redirected to the new MRF functions.

--- +Render/loadSRFCircPol.m
+++ +Render/loadSRFCircPol.m
@@ -4,43 +4,8 @@ function [ theta, x, z, v, sim ] = loadSRFCircPol( filename )
 % each point in space that data exists for. Also returns a struct, sim,
 % describing the parameters of the trap.
 
-fH = fopen(filename, 'r');
-oc = onCleanup(@() fclose(fH));
-
-rf = fread(fH, 1, 'double');
-rabi = fread(fH, 1, 'double');
-qdrpGrad = fread(fH, 1, 'double');
-
-sim.rf = rf; sim.rabi = rabi; sim.qdrpGrad = qdrpGrad;
-
-    function [th, x,z,v] = readStrip(fH)
-        th = fread(fH, 1, 'double');
-        if ~isempty(th);
-            N = fread(fH, 1, 'double');
-            x = fread(fH, N, 'double');
-            z = fread(fH, N, 'double');
-            nV = fread(fH, 1, 'uint32');
-            for i=1:nV
-                v(:,i) = fread(fH, N, 'double');
-            end
-        else
-            N = []; x = []; z = []; v = [];
-        end
-        
-        th = repmat(th, size(x));
-    end
-
-theta = []; x = []; z = []; v = [];
-while ~feof(fH)
-    [tth, tx, tz, tv] = readStrip(fH);
-    theta = [ theta tth ];
-    x = [ x tx ];
-    z = [ z tz ];
-    v = cat(3, v, tv);
-end
-
-% remove non singleton dimensions for ease of use when only one level is required.
-v = squeeze(v);
+warning('deprecated: use loadCircPol');
+[ theta, x, z, v, sim ] = SRFCircPol( filename );
 
 end
 
