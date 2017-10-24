6e0134297e3c8fc3c63e54c1d1e78ae4f387aafb
unknown <bentine@ALWSDAQ71.physics.ox.ac.uk>
2017-10-24 23:28:36 +0100

    Removed 'Calculations' from MRF folder. These calculations include work on the Rabi spectroscopy, which will be useful for thesis. You might want to recover them in the future!

--- +MRF/sortEnergies2.m
+++
@@ -1,7 +0,0 @@
-function [ F ] = sortEnergies2( B, lad )
-
-warning('Change sortEnergies2 to sortEnergies (function renamed). This will be removed in the future');
-F = MRF.sortEnergies(B, lad);
-
-end
-
